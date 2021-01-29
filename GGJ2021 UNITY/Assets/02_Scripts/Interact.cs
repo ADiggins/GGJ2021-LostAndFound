cba58925d4aa4403d75ab7535906ef9deb0b3b07
ADiggins <ajpdiggins@gmail.com>
2021-01-30 10:48:24 +1100

    early Interact and Performance script

---
+++ GGJ2021 UNITY/Assets/02_Scripts/Interact.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+	public KeyCode interactKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+		//Key interactions//
+		if (Input.GetKeyDown(interactKey))
+		{
+			if (other.transform.tag == "Shower")
+			{
+				//trigger shower event
+			}
+			if (other.transform.tag == "Shampoo")
+			{
+				//trigger shampoo event
+			}
+			if (other.transform.tag == "Training")
+			{
+				//trigger training minigame event
+			}
+			if (other.transform.tag == "Mirror")
+			{
+				//trigger mirror event
+			}
+			if (other.transform.tag == "Protection")
+			{
+				//trigger protection event
+			}
+			if (other.transform.tag == "Stink")
+			{
+				//trigger stink event
+			}
+			if (other.transform.tag == "Puddle")
+			{
+				//trigger puddle event
+			}
+			if (other.transform.tag == "Food")
+			{
+				//trigger food event
+			}
+			if (other.transform.tag == "Door")
+			{
+				//trigger door event
+			}
+		}
+
+		//Passive events
+		if (other.transform.tag == "Performance")
+		{
+			other.GetComponentInParent<PerformanceZone>().triggerPerformanceZone();
+		}
+	}
+}
