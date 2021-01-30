7fcffdef1d46f2b54301e1422d709ec14fb5321a
Ben Partridge <ben.partridge249@gmail.com>
2021-01-30 18:11:00 +1100

    Update UI to have a little effect when star count changes

--- GGJ2021 UNITY/Assets/02_Scripts/UI/StarCountUI.cs
+++ GGJ2021 UNITY/Assets/02_Scripts/UI/StarCountUI.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 public class StarCountUI : MonoBehaviour
 {
 
+    private static readonly string ANIM_PARAM_DROP_TEXT = "DropText";
+
     [SerializeField]
     private StarTracker starCount;
 
+    [SerializeField]
+    private TMP_Text dropText;
+
     private TMP_Text text;
+
+    private Animator animator;
+
+    private int cachedNumStars = -1;
 
     private void Awake()
     {
         text = GetComponent<TMP_Text>();
+        animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -22,6 +32,23 @@ public class StarCountUI : MonoBehaviour
 
     private void UpdateStarUI(int numStars)
     {
+        if (cachedNumStars != -1)
+        {
+            var difference = numStars - cachedNumStars;
+            var effectColor = Color.green;
+            var sign = Mathf.Sign(difference);
+            if (sign < 0)
+            {
+                effectColor = Color.red;
+            }
+            var prefix = sign > 0 ? "+" : "-";
+            dropText.text = prefix + Mathf.Abs(difference).ToString();
+            dropText.color = effectColor;
+            animator.SetTrigger(ANIM_PARAM_DROP_TEXT);
+        }
+
         text.text = numStars.ToString() + "/10";
+
+        cachedNumStars = numStars;
     }
 }
