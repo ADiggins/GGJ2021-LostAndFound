9cbeb3b370f8a6de25b7aba7a2b97637cc3b8102
Ben Partridge <ben.partridge249@gmail.com>
2021-01-30 17:27:17 +1100

    Implement victory tracking script
    AllTasksCompletedListener

---
+++ GGJ2021 UNITY/Assets/02_Scripts/AllTasksCompletedListener.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllTasksCompletedListener : MonoBehaviour
+{
+
+    [SerializeField]
+    private StarTracker playerTracker;
+
+    [SerializeField]
+    private StarTracker borisTracker;
+
+    [SerializeField]
+    private StarTracker edwinaTracker;
+
+    [SerializeField]
+    private StarTracker dizzyTracker;
+
+    [SerializeField]
+    private int playerStarVictoryCount;
+
+    [SerializeField]
+    private int borisStarVictoryCount;
+
+    [SerializeField]
+    private int edwinaStarVictoryCount;
+
+    [SerializeField]
+    private int dizzyStarVictoryCount;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void CheckForVictory(int ignored)
+    {
+        if (
+            (int)playerTracker.currentStars == playerStarVictoryCount &&
+            (int)borisTracker.currentStars == borisStarVictoryCount &&
+            (int)edwinaTracker.currentStars == edwinaStarVictoryCount &&
+            (int)dizzyTracker.currentStars == dizzyStarVictoryCount
+        )
+        {
+            DoVictory();
+        }
+    }
+
+    private void DoVictory()
+    {
+        Debug.Log("All tasks completed");
+    }
+
+}
