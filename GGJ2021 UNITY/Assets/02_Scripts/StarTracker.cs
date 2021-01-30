f40c2320180f6f24f5700397efc9cdeefe566ac5
Ben Partridge <ben.partridge249@gmail.com>
2021-01-30 15:51:31 +1100

    Add star counting UI

--- GGJ2021 UNITY/Assets/02_Scripts/StarTracker.cs
+++ GGJ2021 UNITY/Assets/02_Scripts/StarTracker.cs
@@ -4,30 +4,38 @@ using UnityEngine;
 
 public class StarTracker : MonoBehaviour
 {
-	public float currentStars, maxStars, starWorth;
-	private ParticleTrigger pt;
+    public float currentStars, maxStars, starWorth;
+    private ParticleTrigger pt;
 
-	public void Awake()
-	{
-		if (GetComponentInChildren<ParticleTrigger>())
-			pt = GetComponentInChildren<ParticleTrigger>();
-	}
+    public void Awake()
+    {
+        if (GetComponentInChildren<ParticleTrigger>())
+            pt = GetComponentInChildren<ParticleTrigger>();
+    }
 
-	public void AddStars()
-	{
-		currentStars += starWorth;
-		if (currentStars > maxStars)
-			currentStars = maxStars;
-		if (pt != null)
-			pt.Fire();
-		//TODO: Trigger progress UI animation
-	}
+    public void AddStars()
+    {
+        currentStars += starWorth;
+        if (currentStars > maxStars)
+            currentStars = maxStars;
+        if (pt != null)
+            pt.Fire();
+        //TODO: Trigger progress UI animation
+    }
 
-	public void LoseStars()
-	{
-		currentStars -= starWorth;
-		if (currentStars < 0)
-			currentStars = 0;
-		//TODO: Trigger progress UI animation
-	}
+    public void AddStars()
+    {
+        currentStars += starWorth;
+        if (currentStars > maxStars)
+            currentStars = maxStars;
+        //TODO: Trigger progress UI animation
+    }
+
+    public void LoseStars()
+    {
+        currentStars -= starWorth;
+        if (currentStars < 0)
+            currentStars = 0;
+        //TODO: Trigger progress UI animation
+    }
 }
---
+++ GGJ2021 UNITY/Assets/02_Scripts/UI/StarCountUI.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class StarCountUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private StarTracker starCount;
+
+    private TMP_Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        text.text = ((int)starCount.currentStars).ToString();
+    }
+
+}
